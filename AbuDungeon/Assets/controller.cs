be884f53df8c1bb1520e105f42d83f9c7caeab62
evutey <utkgny.50@gmail.com>
2019-10-12 15:46:07 +0300

    Attack

--- AbuDungeon/Assets/controller.cs
+++ AbuDungeon/Assets/controller.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class controller : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class controller : MonoBehaviour
     public float speed;
     public Vector2 moveVelocity;
     public Animator anim;
+    public bool attack;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -21,10 +23,44 @@ public class controller : MonoBehaviour
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
         anim.SetTrigger("jump");
+
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        }
+
+        HandleInput();
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        
+        HandleAttacks();
+        
+        ResetValues();
     }
+
+    public void HandleAttacks()
+    {
+       if(attack && !this.anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+    {
+        anim.SetTrigger("attack");
+        rb.velocity = Vector2.zero;
+    } 
+      
+    }
+     public void HandleInput()
+           {
+               if (Input.GetKeyDown(KeyCode.Z))
+               {
+                   attack = true;
+               }
+           }
+
+     public void ResetValues()
+     {
+         attack = false;
+     }
+    
 }

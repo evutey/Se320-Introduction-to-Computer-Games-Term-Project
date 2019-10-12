7a29a7e8850738a2defddb750f5c3f029b1164dc
Ayberk Ortancalar <ayberkortancalar@programmer.net>
2019-10-13 02:14:41 +0300

    Prosess

---
+++ AbuDungeon/Assets/CharacterController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+
+    public float jumpForce;
+
+    public float moveInput;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+
+    public Animator animator;
+
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private Rigidbody2D rb;
+    private int extraJumps;
+    public int extraJumpsValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis(("Horizontal"));
+        Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (facingRight == false && moveInput > 0 )
+            Flip();
+        else if (facingRight == true && moveInput < 0)
+            Flip();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("speed", Mathf.Abs(moveInput));
+        if (isGrounded == true)
+        {
+            animator.SetBool("isJumping", false);
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            animator.SetBool("isJumping", true);
+            extraJumps--;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+}
---
+++ AbuDungeon/Assets/Controller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 1.23f;
+    public float jumpPower;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+        
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis ("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y );
+        float inputY = Input.GetAxis ("Vertical");
+        
+        
+
+        if (Input.GetKey(KeyCode.Space)){
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        } 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = -transform.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector3(0, 0, 1) * speed;
+        }
+    }
+}

531a2a3566b20a9be109de85f507e9f675d93a26
Ayberk Ortancalar <ayberkortancalar@programmer.net>
2019-10-12 15:13:49 +0300

    uuv

---
+++ AbuDungeon/Assets/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 0.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+        }
+        
+    }
+}
---
+++ AbuDungeon/Assets/controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public Vector2 moveVelocity;
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    { 
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+        anim.SetTrigger("jump");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
